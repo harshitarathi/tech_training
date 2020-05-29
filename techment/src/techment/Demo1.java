f0c2cb232a86d167639c9399ba06f13ecc43a06a
harshitarathi <65978280+harshitarathi@users.noreply.github.com>
2020-05-29 16:30:41 -0300

    second commit

---
+++ techment/src/techment/Demo1.java
@@ -0,0 +1,14 @@
+package techment;
+
+public class Demo1
+{	
+	public static void main(String[] args) {
+	int sum=0;
+	for(int i=1;i<=5;i++) 
+	{
+		sum=sum+i;
+		System.out.println("value of i"+i+"sum"+sum);
+	}
+	System.out.println("sum"+sum);
+	}
+}
